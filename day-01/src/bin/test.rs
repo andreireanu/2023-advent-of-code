8059ef7a6024fdd5386fa91f4926cbddbaea3803
Andrei Ungureanu <andrei.reanu@gmail.com>
2023-12-02 21:08:48 +0200

    Add Day 01 Second Part

--- day-01/src/bin/test.rs
+++ day-01/src/bin/test.rs
@@ -1,7 +1,9 @@
 use std::fs;
 use day_01::process_part1;
+use day_01::process_part2;
 
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
     println!("{}", process_part1(&file));
+    println!("{}", process_part2(&file));
 }
--- day-01/src/lib.rs
+++ day-01/src/lib.rs
@@ -1,5 +1,3 @@
-use core::str::Chars;
-
 pub fn get_first_char(mut line_chars: Vec<char>) -> char {
     let mut iter = line_chars.iter();
     while let Some(c) = iter.next() {
@@ -10,7 +8,60 @@ pub fn get_first_char(mut line_chars: Vec<char>) -> char {
     ' '
 }
 
-pub fn process_part1(input: &String) -> u32 {
+pub fn get_digit(input: &str, reverse: bool) -> u32 {
+    let arr = ["one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9"];
+    let mut out : String = String::new();
+    if reverse == false {
+        let mut ref_idx = usize::MAX;
+        for el in arr.iter() {
+            match input.find(el) {
+                Some(idx) => {
+                    if idx < ref_idx {
+                        ref_idx = idx;
+                        out = el.to_string();
+                    }
+                },
+                None => {}
+            }
+        }
+    } else {
+        let mut ref_idx = 0;
+        for el in arr.iter() {
+            match input.rfind(el) {
+                Some(idx) => {
+                    if idx >= ref_idx {
+                        ref_idx = idx;
+                        out = el.to_string();
+                    }
+                },
+                None => {}
+            }
+        }
+    }
+    match out.as_str() {
+        "one"  => 1,
+        "1" => 1,
+        "two" => 2,
+        "2" => 2,
+        "three" => 3,
+        "3" => 3,
+        "four" => 4,
+        "4" => 4,
+        "five" => 5,
+        "5" => 5,
+        "six" => 6,
+        "6" => 6,
+        "seven" => 7,
+        "7" => 7,
+        "eight" => 8,
+        "8" => 8,
+        "nine" => 9,
+        "9" => 9,
+        _ => 0,
+    }
+}
+
+pub fn process_part1(input: &str) -> u32 {
     input
         .split("\n")
         .map(|line|
@@ -23,5 +74,46 @@ pub fn process_part1(input: &String) -> u32 {
                   calibration_value
               })
         .sum()
+}
+
+pub fn process_part2(input: &str) -> u32 {
+    input
+        .split("\n")
+        .map(|line|
+            {
+                let first_digit = get_digit(line, false);
+                let last_digit = get_digit(line, true);
+                let out = first_digit * 10 + last_digit;
+                out
+            })
+        .sum()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_first() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let result = process_part1(&input);
+        assert_eq!(result, 142);
+    }
+    #[test]
+    fn test_second() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let result = process_part2(&input);
+        assert_eq!(result, 281);
+    }
 }
 
