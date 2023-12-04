cdae17b5785d64e33ad61056783d8f9f293fe111
Andrei Ungureanu <andrei.reanu@gmail.com>
2023-12-04 21:59:17 +0200

    Add Day02 Part 2

--- day-02/src/lib.rs
+++ day-02/src/lib.rs
@@ -10,7 +10,7 @@ pub fn process_part1(input: &str) -> u32 {
                 let draw: Vec<&str> = set.split(", ").collect();
                 for pick in draw {
                     let (number_string, color) = pick.split_once(" ").unwrap();
-                    let number = number_string.parse::<i32>().unwrap();
+                    let number = number_string.parse::<u32>().unwrap();
                     match color {
                         "red" => {
                             if number > 12 {
@@ -37,6 +37,32 @@ pub fn process_part1(input: &str) -> u32 {
         .sum()
 }
 
+pub fn process_part2(input: &str) -> u32 {
+    input
+        .split("\n")
+        .map(|line| {
+            let mut cubes = [0, 0, 0];
+            let (_, game_data) = line.split_once(": ").unwrap();
+            let sets: Vec<&str> = game_data.split("; ").collect();
+            for set in sets {
+                let draw: Vec<&str> = set.split(", ").collect();
+                for pick in draw {
+                    let (number_string, color) = pick.split_once(" ").unwrap();
+                    let number = number_string.parse::<u32>().unwrap();
+                    match color {
+                        "red" => cubes[0] = number.max(cubes[0]),
+                        "green" => cubes[1] = number.max(cubes[1]),
+                        "blue" => cubes[2] = number.max(cubes[2]),
+                        _ => unreachable!(),
+                    }
+                }
+            }
+            cubes[0] * cubes[1] * cubes[2]
+        }
+        )
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,4 +77,16 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
         let result = process_part1(&input);
         assert_eq!(result, 8);
     }
+
+    #[test]
+    fn test_second() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let result = process_part2(&input);
+        assert_eq!(result, 2286);
+    }
+
 }
