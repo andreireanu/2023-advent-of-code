8d906b250267cd7eb3707b7993b5cffd81b04905
Andrei Ungureanu <andrei.reanu@gmail.com>
2023-12-10 14:19:22 +0200

    Add Day03 Part1

--- day-03/src/lib.rs
+++ day-03/src/lib.rs
@@ -1,12 +1,40 @@
-use core::str::Chars;
+use core::cmp::max;
+use core::cmp::min;
 
-pub enum CharType {
-    Start,
-    Middle,
-    End
-}
-pub fn eval_neighbour(idx: usize, char_type: CharType, previous: &Option<Chars>, current: &Chars,  next: &Option<Chars>, ) {
+pub fn eval_neighbours(line_length: usize, start_idx: usize, end_idx: usize, previous: Option<&Vec<char>>, current: &Vec<char>,  next:  Option<&Vec<char>>) -> bool {
 
+    let eval_start: isize = max(0, start_idx as isize -1);
+    let eval_end: isize = min(end_idx as isize,line_length as isize);
+
+    if let Some(previous_line) = previous {
+        for i in eval_start..=eval_end {
+            match previous_line[i as usize] {
+                '.' | '0'..='9' => { continue; }
+                _ => { return true}
+            }
+        }
+    }
+
+    match current[eval_start as usize] {
+        '.' | '0'..='9' => { }
+        _ => { return true}
+    }
+
+    match current[eval_end as usize] {
+        '.' | '0'..='9' => { }
+        _ => { return true}
+    }
+
+    if let Some(next_line) = next {
+        for i in eval_start..=eval_end {
+            match next_line[i as usize] {
+                '.' | '0'..='9' => { continue; }
+                _ => { return true}
+            }
+        }
+    }
+
+    false
 }
 
 pub fn process_part1(input: &str) -> u32 {
@@ -14,17 +42,47 @@ pub fn process_part1(input: &str) -> u32 {
         .split("\n")
         .map(|line| line.chars().collect())
         .collect();
-    println!("{:?}", lines);
     let mut previous_line: Option<&Vec<char>> = None;
-    let mut next_line: Option<&Vec<char>> = Some(&lines[0]);
-    let line_length = lines[0].len();
-    let line_sum = 0;
+    let mut next_line: Option<&Vec<char>> = Some(&lines[1]);
+    let line_length= lines[0].len();
+    let mut sum = 0;
     for i in 0..lines.len() {
-        println!("{}",i);
-        println!("{:?}", previous_line);
-        println!("{:?}", lines[i]);
-        println!("{:?}", next_line);
+        let mut start_idx = 0;
+        let mut end_idx;
+        let mut is_digit = false;
+        let mut is_valid;
+        let mut current_num = 0;
+        for idx in 0..line_length  {
+            match lines[i][idx]  {
+                '0'..='9' => {
+                    current_num = current_num * 10 + lines[i][idx].to_digit(10).unwrap();
+                    if is_digit == false {
+                        is_digit = true;
+                        start_idx = idx;
+                    };
+                    if idx == line_length - 1 {
+                        end_idx = idx;
+                        is_valid = eval_neighbours(line_length, start_idx, end_idx, previous_line, &lines[i], next_line );
+                        if is_valid {
+                            sum += current_num;
+                        }
+                        current_num = 0;
+                    }
 
+                }
+                _ => {
+                    if is_digit == true {
+                        is_digit = false;
+                        end_idx = idx;
+                        is_valid = eval_neighbours(line_length, start_idx, end_idx, previous_line, &lines[i], next_line );
+                        if is_valid {
+                            sum += current_num;
+                        }
+                        current_num = 0;
+                    };
+                }
+            }
+        }
 
         if i >= lines.len() - 2 {
             next_line = None;
@@ -32,12 +90,9 @@ pub fn process_part1(input: &str) -> u32 {
             next_line = Some(&lines[i+2]);
         }
         previous_line = Some(&lines[i]);
-        println!("----------");
     }
-    32
+    sum
 }
-
-
 
 
 #[cfg(test)]
@@ -46,24 +101,18 @@ mod tests {
 
     #[test]
     fn test_first() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
         let result = process_part1(&input);
-        assert_eq!(result, 8);
-    }
-
-    #[test]
-    fn test_second() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        let result = process_part2(&input);
-        assert_eq!(result, 2286);
+        assert_eq!(result, 4361);
     }
 
 }
