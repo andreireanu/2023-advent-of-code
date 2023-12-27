634023aeda1bba0fea615e29d030a425ea1f5858
Andrei Ungureanu <andrei.reanu@gmail.com>
2023-12-27 10:36:33 +0200

    Add Day08 Part1

--- day-08/src/bin/test.rs
+++ day-08/src/bin/test.rs
@@ -2,6 +2,6 @@ use std::fs;
 use day_08::process_part1;
 
 fn main() {
-    let file = fs::read_to_string("./inputDemo.txt").unwrap();
+    let file = fs::read_to_string("./input.txt").unwrap();
     println!("{}", process_part1(&file));
 }
--- day-08/src/lib.rs
+++ day-08/src/lib.rs
@@ -2,13 +2,30 @@ use std::collections::HashMap;
 
 pub fn process_part1(input: &str) -> i64 {
 
-    input
+    let (nodes, map) = input.split_once("\n\n").unwrap();
+    let mut hm = HashMap::new();
+    map
         .split("\n")
         .for_each(|line| {
-            println!("{}", line);
+            let (node, left_right) = line.split_once(" = ").unwrap();
+            let left = &left_right[1..4];
+            let right = &left_right[6..9];
+            hm.insert(node, (left,right));
         });
-
-    42
+    let mut count = 0;
+    let mut current = "AAA";
+    while current != "ZZZ" {
+        for c in nodes.chars() {
+            count += 1;
+            let left_right = hm.get(&current).unwrap();
+            if c == 'L'{
+                current = left_right.0;
+            } else {
+                current = left_right.1;
+            }
+        }
+    }
+    count
 }
 
 ////////////////////////////////////////
@@ -21,23 +38,28 @@ mod tests {
     use super::*;
     #[test]
     fn test_first() {
-        let input = "32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483";
+        let input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
         let result = process_part1(&input);
-        assert_eq!(result, 6440);
+        assert_eq!(result, 2);
     }
 
-    fn test_second() {
-        let input = "32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483";
-        let result = process_part2(&input);
-        assert_eq!(result, 5905);
+    #[test]
+    fn test_first_2nd_example() {
+        let input = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+        let result = process_part1(&input);
+        assert_eq!(result, 6);
     }
 
 }
