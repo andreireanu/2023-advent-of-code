5d039973766842d48deb0552ffd9f496d630b3e1
Andrei Ungureanu <andrei.reanu@gmail.com>
2024-01-08 09:57:24 +0200

    Add Day12 Part2

--- day-12/src/bin/test.rs
+++ day-12/src/bin/test.rs
@@ -1,8 +1,10 @@
 use std::fs;
 use day_12::process_part1;
+use day_12::process_part2;
 
 
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
     println!("{}", process_part1(&file));
+    println!("{}", process_part2(&file));
 }
--- day-12/src/lib.rs
+++ day-12/src/lib.rs
@@ -1,40 +1,38 @@
 use std::collections::HashMap;
 
-pub fn calculate(previous: char, i: usize, j: usize, current_damaged: usize, springs: &Vec<char>, damaged: &Vec<usize>, cache: &mut HashMap<(char, usize, usize), i32> ) -> i32 {
+pub fn calculate(previous: char, i: usize, j: usize, current_damaged: usize, springs: &Vec<char>, damaged: &Vec<usize>, cache: &mut HashMap<(char, usize, usize, usize), i64> ) -> i64 {
 
-    if let Some(val) = cache.get(&(previous,i,j)) {
-        println!("Found {:?}: {}", (previous,i,j), *val);
-        return *val;
+    // Get recorded cache value
+    if i < springs.len() {
+        if let Some(val) = cache.get(&(previous, current_damaged,i,j)) {
+            return *val;
+        }
     }
 
+    // Too many contiguous groups
     if j == damaged.len() {
         return 0;
     }
 
+    // If end of the line
     if i == springs.len() {
+        // Too few contiguous groups
         if j < damaged.len() - 1 {
             return 0;
         }
-        println!("{} {} {} {}", i, j, current_damaged, damaged[j]);
+        // check last contiguous group
         return match current_damaged == damaged[j] {
-            true => {
-                println!("OK END");
-                1}
-            false => {
-                println!("FALSE END");
-                0
-                }
+            true => 1,
+            false => 0
             }
     }
-
 
     let out = match springs[i] {
         '.' => {
                 match previous {
                     '#' => {
+                        // End of contiguous group, check if len ok
                         if current_damaged != damaged[j] {
-                            println!("FAIL #.");
-                            println!("{} {} {} {}", i, j, current_damaged, damaged[j]);
                             return 0;
                         } else {
                             return calculate('.', i+1, j, current_damaged, springs, damaged, cache)   ;
@@ -57,39 +55,26 @@ pub fn calculate(previous: char, i: usize, j: usize, current_damaged: usize, spr
             }
         }
         '?' =>  {
+            // if current = '?', we can replace it with either a '.' or a '#'
             match previous {
                 '.' => {
-                    let res_dot = {
-                        println!("HERE1");
-                        calculate('.', i + 1, j, current_damaged, springs, damaged, cache)
-                    };
-                    let res_hash = {
-                        println!("HERE2");
-                        calculate('#', i + 1, j + 1, 1, springs, damaged, cache)
-                        };
+                    let res_dot = calculate('.', i + 1, j, current_damaged, springs, damaged, cache);
+                    let res_hash = calculate('#', i + 1, j + 1, 1, springs, damaged, cache);
                     res_dot + res_hash
                 }
                 '#' => {
                     let res_dot = {
-                        println!("HERE3");
+                        // End of contiguous group, check if len ok
                         if j == damaged.len() {
-                            println!("PASSED LEN");
                             return 0;
                         }
-                        println!("{} {} {} {}", i, j, current_damaged, damaged[j]);
-
                         if current_damaged != damaged[j] {
-                            println!("FAIL .#");
-                            println!("{} {} {} {}", i, j, current_damaged, damaged[j]);
                             0
                         } else {
-                            println!("Continue");
                             calculate('.', i + 1, j, current_damaged, springs, damaged, cache)
                         }
                     };
                     let res_hash = {
-                        println!("HERE4");
-                        println!("{} {} {} {}", i, j, current_damaged, damaged[j]);
                         calculate('#', i + 1, j, current_damaged + 1, springs, damaged, cache)
                     };
                     res_dot + res_hash
@@ -99,13 +84,14 @@ pub fn calculate(previous: char, i: usize, j: usize, current_damaged: usize, spr
         }
         _ => unreachable!(),
     };
-    // cache.insert  ((previous,i,j), out);
+    // Insert into cache
+    cache.insert((previous, current_damaged,i,j), out);
     out
 }
 
 
 
-pub fn process_part1(input: &str) -> i32 {
+pub fn process_part1(input: &str) -> i64 {
     let out = input
         .split("\n")
         .map(|line| {
@@ -113,18 +99,35 @@ pub fn process_part1(input: &str) -> i32 {
             let springs = springs_str.chars().collect::<Vec<char>>();
             let mut damaged = vec![0];
             damaged.extend(damaged_str.split(',').map(|nr| nr.parse().unwrap()).collect::<Vec<usize>>());
-            println!("{}", springs.len());
-            println!("{:?} {:?}", springs, damaged);
-            let mut cache: HashMap<(char, usize, usize), i32> = HashMap::new();
+            let mut cache: HashMap<(char, usize, usize, usize), i64> = HashMap::new();
             let out = calculate('.', 0, 0, 0, &springs, &damaged, &mut cache );
-            for (k,v) in cache {
-                println!("{:?} {}", k, v);
-            }
             out
         }).sum();
     out
 }
 
+pub fn process_part2(input: &str) -> i64 {
+    let out = input
+        .split("\n")
+        .map(|line| {
+            let (springs_str, damaged_str) = line.split_once(' ').unwrap();
+            let springs_single = springs_str.chars().collect::<Vec<char>>();
+            let damaged_single = damaged_str.split(',').map(|nr| nr.parse().unwrap()).collect::<Vec<usize>>();
+            let mut springs= vec![];
+            let mut damaged = vec![0];
+            for _ in 0..4 {
+                springs.extend(springs_single.clone());
+                springs.push('?');
+                damaged.extend(damaged_single.clone());
+            }
+            springs.extend(springs_single.iter().cloned());
+            damaged.extend(damaged_single.clone());
+            let mut cache: HashMap<(char, usize, usize, usize), i64> = HashMap::new();
+            let out = calculate('.', 0, 0, 0, &springs, &damaged, &mut cache );
+            out
+        }).sum();
+    out
+}
 
 ////////////////////////////////////////
 
@@ -144,6 +147,16 @@ mod tests {
         assert_eq!(result, 21);
     }
 
-
+    #[test]
+    fn test_second() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        let result = process_part2(&input);
+        assert_eq!(result, 525152);
+    }
 
 }
