e4de03e86373af33603b706baa44c97b38df49ee
Andrei Ungureanu <andrei.reanu@gmail.com>
2024-01-10 21:23:02 +0200

    Add Day12 Part2

--- day-13/src/lib.rs
+++ day-13/src/lib.rs
@@ -23,30 +23,114 @@ pub fn check_reflection(v: Vec<String>) -> i64 {
     -1
 }
 
+pub fn evaluate_grid(grid: &str) -> (i64, char) {
+    let v_horizontal: Vec<String> = grid
+        .split('\n')
+        .map(|element| element.to_string())
+        .collect();
+    let h_reflex = check_reflection(v_horizontal.clone());
+    if h_reflex != -1 {
+        return (100 * (h_reflex + 1),'h');
+    } else {
+
+        let v_vertical: Vec<String> = (0..v_horizontal[0].len())
+            .map(|col| {  v_horizontal.iter().map(|line| &line[col..col + 1]).collect() })
+            .collect();
+
+        let v_reflex = check_reflection(v_vertical);
+        (v_reflex + 1, 'v')
+    }
+}
+
+
+
 pub fn process_part1(input: &str) -> i64 {
     input
         .split("\n\n")
         .map(|grid| {
-            let mut v_horizontal: Vec<String> = Vec::new();
-            v_horizontal = grid
+            evaluate_grid(grid).0
+        })
+        .sum()
+}
+
+//////////////////////////////////
+
+pub fn compare_lines(line1: &str, line2: &str) -> bool {
+    let mut count_smudges = 0;
+    let line1_chars = line1.as_bytes();
+    let line2_chars = line2.as_bytes();
+    for idx in 0..line1.len() {
+        if  line1_chars[idx] != line2_chars[idx]{
+            count_smudges += 1;
+        }
+    }
+    if count_smudges <= 1 {
+        return true;
+    }
+    false
+}
+
+pub fn check_reflection_smudge(v: Vec<String>, dir: char, part1: &(i64,char)  )  -> i64 {
+    let mut reflex_candidates = vec![];
+    for i in 0..v.len()-1 {
+        if compare_lines(&v[i],&v[i+1]) {
+            reflex_candidates.push(i);
+        }
+    }
+    for reflex_start in reflex_candidates {
+        let mut reflexed = true;
+        let mut idx: usize = 1;
+        while reflex_start as isize - idx as isize >= 0 && reflex_start + idx + 1 < v.len() {
+            if compare_lines(&v[reflex_start - idx],&v[reflex_start + idx + 1]) == false {
+                reflexed = false;
+                break;
+            } else {
+                idx += 1;
+            };
+        };
+        if reflexed == true {
+            match (dir, part1.1) {
+                ('h' , 'h') => {
+                    if reflex_start != (part1.0 as usize / 100  ) - 1 {
+                       return reflex_start as i64;
+                    }
+                },
+                ('v' , 'v') => {
+                    if reflex_start !=  part1.0 as usize - 1 {
+                    return reflex_start as i64;
+                }},
+                (_,_) => {return reflex_start as i64;}
+            }
+        }
+    }
+    -1
+}
+
+pub fn process_part2(input: &str) -> i64 {
+    input
+        .split("\n\n")
+        .map(|grid| {
+            let v_horizontal: Vec<String> = grid
                 .split('\n')
                 .map(|element| element.to_string())
                 .collect();
-            let h_reflex = check_reflection(v_horizontal.clone());
+            let part1 = evaluate_grid(grid);
+            let h_reflex = check_reflection_smudge(v_horizontal.clone(), 'h', &part1);
             if h_reflex != -1 {
                 return 100 * (h_reflex + 1);
             } else {
+                let v_vertical: Vec<String> = (0..v_horizontal[0].len())
+                    .map(|col| {  v_horizontal.iter().map(|line| &line[col..col + 1]).collect() })
+                    .collect();
 
-            let v_vertical: Vec<String> = (0..v_horizontal[0].len())
-                .map(|col| {  v_horizontal.iter().map(|line| &line[col..col + 1]).collect() })
-                .collect();
-
-            let v_reflex = check_reflection(v_vertical);
-            v_reflex + 1
+                let v_reflex = check_reflection_smudge(v_vertical, 'v', &part1);
+                v_reflex + 1
             }
         })
         .sum()
 }
+
+
 
 ////////////////////////////////////////
 
@@ -73,6 +157,26 @@ mod tests {
 #....#..#";
         let result = process_part1(&input);
         assert_eq!(result, 405);
+    }
+    #[test]
+    fn test_second() {
+        let input = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+        let result = process_part2(&input);
+        assert_eq!(result, 400);
     }
 
 }
